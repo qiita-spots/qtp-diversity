a430339b345175eb9017feeefe5c2eb60406c9aa
Jose Navas <josenavasmolina@gmail.com>
2017-02-15 07:26:58 -0800

    Skeleton code

---
+++ qtp_diversity/__init__.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The Qiita Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from qiita_client import QiitaTypePlugin, QiitaArtifactType
+
+from .validate import validate
+from .summary import generate_html_summary
+
+# Define the supported artifact types
+artifact_types = [
+    QiitaArtifactType('distance_matrix',
+                      'Distance matrix holding pairwise distance between '
+                      'samples', False, False, [('directory', True)]),
+    QiitaArtifactType('rarefaction_curves',
+                      'Rarefaction curves', False, False,
+                      [('directory', True)]),
+    QiitaArtifactType('taxa_summary',
+                      'Taxa summary plots', False, False,
+                      [('directory', True)])]
+
+# Initialize the plugin
+plugin = QiitaTypePlugin('Diversity types', '0.1.0',
+                         'Diversity artifacts type plugin',
+                         validate, generate_html_summary,
+                         artifact_types)
---
+++ qtp_diversity/summary.py
@@ -0,0 +1,32 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The Qiita Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+
+def generate_html_summary(qclient, job_id, parameters, out_dir):
+    """Generates the HTML summary of an artifact
+
+    Parameters
+    ----------
+    qclient : qiita_client.QiitaClient
+        The Qiita server client
+    job_id : str
+        The job id
+    parameters : dict
+        The parameter values to validate and create the artifact
+    out_dir : str
+        The path to the job's output directory
+
+    Returns
+    -------
+    bool, None, str
+        Whether the job is successful
+        Ignored
+        The error message, if not successful
+    """
+    # TODO
+    return True, None, ""
