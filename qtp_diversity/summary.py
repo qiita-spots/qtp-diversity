77918bdccf468be4a4c426235e8c31ed7a154e6c
Jose Navas <josenavasmolina@gmail.com>
2017-06-16 10:13:34 -0400

    Initial steps for adding the summary

--- qtp_diversity/summary.py
+++ qtp_diversity/summary.py
@@ -6,6 +6,61 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
+from os.path import join
+from urllib.parse import quote
+from base64 import b64encode
+from io import BytesIO
+
+import numpy as np
+import pandas as pd
+from emperor import Emperor
+from skbio import OrdinationResults, DistanceMatrix
+
+
+DM_HTML = """<b>Number of samples:</b> %d</br>
+<b>Minimum distance:</b> %.4f<br/>
+<b>Maximum distance:</b> %.4f<br/>
+<b>Mean distance:</b> %.4f<br/>
+<b>Median distance:</b> %.4f<br/>
+<br/><hr/><br/>
+<img src = "%s"/>"""
+
+
+def _generate_distance_matrix_summary(files, metadata, out_dir):
+    # Magic number [0] -> there is only one plain text file which is
+    # the distance matrix
+    dm = DistanceMatrix.read(files['plain_text'][0])
+    data = dm.condensed_form()
+
+    # Generate a heatmap with the distance matrix
+    sc_plot = BytesIO()
+    fig = dm.plot()
+    fig.savefig(sc_plot, format='png')
+    sc_plot.seek(0)
+    uri = 'data:image/png;base64,' + quote(b64encode(sc_plot.getbuffer()))
+
+    with open(join(out_dir, 'index.html'), 'w') as f:
+        f.write(DM_HTML % (dm.shape[0], data.min(), data.max(),
+                           data.mean(), np.median(data), uri))
+
+
+def _generate_ordination_results_summary(files, metadata, out_dir):
+    # Magic number [0] -> there is only one plain text file and it is the
+    # ordination results
+    ord_res = OrdinationResults.read(files['plain_text'][0])
+    md_df = pd.DataFrame.from_dict(metadata, orient='index')
+    emp = Emperor(ord_res, md_df, remote=".")
+
+    with open(join(out_dir, 'index.html'), 'w') as f:
+        f.write(emp.make_emperor(standalone=True))
+        emp.copy_support_files(out_dir)
+
+
+HTML_SUMMARIZERS = {
+    'distance_matrix': _generate_distance_matrix_summary,
+    'ordination_results': _generate_ordination_results_summary
+}
+
 
 def generate_html_summary(qclient, job_id, parameters, out_dir):
     """Generates the HTML summary of an artifact
@@ -28,5 +83,15 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
         Ignored
         The error message, if not successful
     """
-    # TODO
+    artifact_id = parameters['input_data']
+    qclient_url = "/qiita_db/artifacts/%s/" % artifact_id
+    artifact_info = qclient.get(qclient_url)
+    atype = artifact_info['type']
+
+    if atype not in HTML_SUMMARIZERS:
+        return (False, None, "Unknown artifact type %s. Supported types: %s"
+                             % (atype, ", ".join(sorted(HTML_SUMMARIZERS))))
+
+    summary_fp = HTML_SUMMARIZERS[atype](artifact_info['files'], metadata,
+                                         out_dir)
     return True, None, ""
--- qtp_diversity/tests/test_summary.py
+++ qtp_diversity/tests/test_summary.py
@@ -8,12 +8,56 @@
 
 
 from unittest import main
+from tempfile import mkdtemp, mkstemp
+from os.path import exists, isdir
+from os import remove, close
+from shutil import rmtree
 
 from qiita_client.testing import PluginTestCase
+from skbio import DistanceMatrix
+
+from qtp_diversity.summary import (
+    _generate_distance_matrix_summary, _generate_ordination_results_summary,
+    generate_html_summary)
 
 
 class SummaryTests(PluginTestCase):
-    pass
+    def setUp(self):
+        self.out_dir = mkdtemp()
+        self._clean_up_files = [self.out_dir]
+        self.metadata = {
+            '1.SKM4.640180': {'col': "group1"},
+            '1.SKB8.640193': {'col': "group1"},
+            '1.SKD8.640184': {'col': "group2"},
+            '1.SKM9.640192': {'col': "group2"},
+            '1.SKB7.640196': {'col': "group2"}}
+
+    def tearDown(self):
+        for fp in self._clean_up_files:
+            if exists(fp):
+                if isdir(fp):
+                    rmtree(fp)
+                else:
+                    remove(fp)
+
+    def test_generate_distance_matrix_summary(self):
+        dm = DistanceMatrix([[0.0, 0.850, 0.250],
+                             [0.850, 0.0, 0.500],
+                             [0.250, 0.500, 0.0]])
+        fd, fp = mkstemp(suffix='.txt', dir=self.out_dir)
+        close(fd)
+        dm.write(fp)
+        obs = _generate_distance_matrix_summary(
+            {'plain_text': [fp]}, self.metadata, self.out_dir)
+        exp = []
+        self.assertEqual(obs, exp)
+
+    def test_generate_ordination_results_summary(self):
+        _generate_ordination_results_summary()
+
+    def test_generate_html_summary(self):
+        generate_html_summary()
+
 
 if __name__ == '__main__':
     main()
